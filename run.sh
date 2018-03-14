deb543b72ebee15f88c34085353997b5e4a5999a
Naum Azeredo <naumazeredo@gmail.com>
2018-03-14 02:00:07 +0000

    Fix database connection bug

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/venv/bin/activate
 source $DIR/.ENV
 
-$DIR/bot.py --noauth_local_webserver
+cd $DIR
+
+./bot.py --noauth_local_webserver
